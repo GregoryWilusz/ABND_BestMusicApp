456bfe3ca6bfcb9c3fbadb969b2d1c45c8371593
Greg <wilusz.gregory@gmail.com>
2018-04-18 14:29:13 +0200

    Added Intents and dummy xmls

--- app/src/main/java/com/example/android/bestmusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/bestmusicapp/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.android.bestmusicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.albums) TextView albums_category;
+    @BindView(R.id.playlist) TextView playlist_category;
+    @BindView(R.id.now_playing) TextView now_playing_category;
+    @BindView(R.id.search) TextView search_category;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        albums_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent albumsIntent = new Intent(MainActivity.this, AlbumsActivity.class);
+
+                startActivity(albumsIntent);
+            }
+        });
+
+        playlist_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playlistIntent = new Intent(MainActivity.this, PlaylistActivity.class);
+
+                startActivity(playlistIntent);
+            }
+        });
+
+        now_playing_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nowPlayingIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+
+                startActivity(nowPlayingIntent);
+            }
+        });
+
+        search_category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+
+                startActivity(searchIntent);
+            }
+        });
+
     }
+
+
 }
