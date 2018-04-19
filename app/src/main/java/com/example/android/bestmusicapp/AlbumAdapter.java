28eeca523a2465ea957705e9492080693b006037
Greg <wilusz.gregory@gmail.com>
2018-04-19 12:53:56 +0200

    Added layouts for further album details, properly implemented ButterKnife inside adapter

--- app/src/main/java/com/example/android/bestmusicapp/AlbumAdapter.java
+++ app/src/main/java/com/example/android/bestmusicapp/AlbumAdapter.java
@@ -7,40 +7,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AlbumAdapter extends ArrayAdapter<Album> {
-//    @BindView(R.id.artist_name_text_view) TextView artistNameTextView;
-//    @BindView(R.id.album_name_text_view) TextView albumNameTextView;
 
     public AlbumAdapter(@NonNull Context context, ArrayList<Album> albums) {
         super(context, 0, albums);
     }
 
+    static class ViewHolder {
+        @BindView(R.id.artist_name_text_view) TextView artistNameTextView;
+        @BindView(R.id.album_name_text_view) TextView albumNameTextView;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View listItemView = convertView;
-        if(listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.album_list_item, parent, false);
+        ViewHolder holder;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.album_list_item, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
         }
 
         Album currentAlbum = getItem(position);
 
-        TextView albumNameTextView = (TextView) listItemView.findViewById(R.id.album_name_text_view);
-        albumNameTextView.setText(currentAlbum.getAlbumName());
+        holder.albumNameTextView.setText(currentAlbum.getAlbumName());
+        holder.artistNameTextView.setText(currentAlbum.getArtistName());
 
-        TextView artistNameTextView = (TextView) listItemView.findViewById(R.id.artist_name_text_view);
-        artistNameTextView.setText(currentAlbum.getArtistName());
-
-
-        return listItemView;
+        return convertView;
     }
-
-
 }
--- app/src/main/java/com/example/android/bestmusicapp/AlbumsActivity.java
+++ app/src/main/java/com/example/android/bestmusicapp/AlbumsActivity.java
@@ -6,26 +6,30 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class AlbumsActivity extends AppCompatActivity {
+
+    @BindView(R.id.album_list) ListView albumListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.albums_list);
+        ButterKnife.bind(this);
 
         ArrayList<Album> albums = new ArrayList<Album>();
 
         albums.add(new Album("Michael Jackson", "Moon Walking", "blabla",
                 new Song(1, "Milk", 3)));
-        albums.add(new Album("Michael Jacksons", "Moon Walkingd", "blablaa",
+        albums.add(new Album("Michael Jackson", "Moon", "blablaa",
                 new Song(1, "Song 1", 4)));
-        albums.add(new Album("Michael Jacksonss", "Moon Walkings", "blablaaa",
+        albums.add(new Album("Michael Jackson", "Thriller", "blablaaa",
                 new Song(1, "Song 1", 4)));
 
         AlbumAdapter adapter = new AlbumAdapter(this, albums);
-
-        ListView albumListView = (ListView) findViewById(R.id.album_list);
         albumListView.setAdapter(adapter);
 
     }
